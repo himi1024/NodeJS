dfd394ce123d03efb44e7537d85bfac34a592287
himi1024 <fhchanaa@connect.ust.hk>
2023-07-30 16:24:11 +0800

    Creating JSON Object in JS and send it through by GET

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # NodeJS
 nodeJS with mongoDB Atlas
 
+# install packages
 npm i express
 npm i uuid
 
+# auto update after everytime when there is a code changing
+change the package.json
+"start": "npx nodemon src/app.js"
+# execute
 npm run start
--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Creating a json object in JS 
+const json = {
+    name: "Himi Chan",
+    Color: "Blue",
+    Numbers: [
+        10, 24
+    ]
+};
+
+// JSON List of People
+const people = [
+    {
+        "name": "Ada",
+        "age": "20"
+    },
+    {
+        "name": "Leon",
+        "age": "22"
+    },
+    {
+        "name": "Ashley",
+        "age": "20"
+    }
+];
+
 // Create Endpoint
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.end("Hello World!");
+})
+
+app.get('/api/people', (req, res) => {
+    res.send({"people": people});
 });
+
+app.post('/', (req, res) => {
+    res.send('POST request');
+})
 
 app.listen(PORT, () => {
     console.log('App listening on port ' + PORT);
