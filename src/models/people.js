ec1d06e62db7fe22b0268caffca217184b3b4e88
himi1024 <fhchanaa@connect.ust.hk>
2023-09-06 14:44:31 +0800

    Use ObjectID to get the data

--- src/models/people.js
+++ src/models/people.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Define a model schema
 const memberSchema = new mongoose.Schema({
+    // To set the name field as a compulsory variable to be filled in.
     name: {
         type: String,
         required: true
